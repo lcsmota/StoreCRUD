192df891069f41e04a515f86881c270bfc31c030
lcsmota <sacul_m@protonmail.com>
2022-11-28 09:11:20 -0300

    Register DI

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Store.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddDbContext<StoreDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+    builder.Services.AddScoped<StoreDbContext, StoreDbContext>();
+
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();

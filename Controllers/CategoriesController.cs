285c0b3c3c32cc98df679fbc1b195c1ddba1e412
lcsmota <sacul_m@protonmail.com>
2022-11-28 11:20:33 -0300

    Add Category controller

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Store.Context;
+using Store.Models;
+
+namespace Store.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Category>>> GetCategoriesAsync(StoreDbContext context)
+    {
+        var categories = await context.Categories.AsNoTracking().ToListAsync();
+
+        return Ok(categories);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Category>> GetCategoryAsync(
+        int id,
+        StoreDbContext context)
+    {
+        var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+        if (category is null) return NotFound("Categoria não encontrada.");
+
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Category>> CreateCategoryAsync(
+        StoreDbContext context,
+        Category model)
+    {
+        try
+        {
+            context.Categories.Add(model);
+            await context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+        catch (System.Exception)
+        {
+            return BadRequest("Erro ao tentar cadastrar categoria.");
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateCategoryAsync(
+        int id,
+        Category model,
+        StoreDbContext context)
+    {
+        if (model.Id != id) return BadRequest("Verifique os dados e tente novamente.");
+
+        try
+        {
+            context.Entry<Category>(model).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        catch (System.Exception)
+        {
+            return BadRequest("Erro ao tentar atualizar categoria.");
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteCategoryAsync(int id, StoreDbContext context)
+    {
+        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (category is null) return NotFound("Categoria não encontrada.");
+
+        try
+        {
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        catch (System.Exception)
+        {
+            return BadRequest("Erro ao tentar remover categoria");
+        }
+    }
+}

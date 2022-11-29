663dc58f89bce404dc786f4cdf21ecc91b91b4a0
lcsmota <sacul_m@protonmail.com>
2022-11-29 15:17:37 -0300

    Fix bug

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddDbContext<StoreDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-    builder.Services.AddScoped<StoreDbContext, StoreDbContext>();
-
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();
